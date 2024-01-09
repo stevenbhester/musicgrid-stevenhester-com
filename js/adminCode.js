1cfa2c9ab6104af39a7edd89acccd27856b2739f
Steven Hester <45438379+stevenbhester@users.noreply.github.com>
2024-01-08 18:45:09 -0800

    Update adminCode.js

--- js/adminCode.js
+++ js/adminCode.js
@@ -236,6 +236,7 @@ async function calculateAnswerScores(answersUnscored, gridId) {
   let normedAnswerScore = 0;
   const answersWithScores = [];
   let songObserved = "";
+  let songLoad = "";
   let popularityObserved = 0;
   let gridIdString = gridId.toString();
   
@@ -255,6 +256,7 @@ async function calculateAnswerScores(answersUnscored, gridId) {
       } else {
         normedAnswerScore = 6+5*Math.round(10*(1 - ((popularityObserved - fieldScoreMin)/(fieldScoreMax - fieldScoreMin))))/10;
       }
+      songLoad = songObserved.replaceAll("'","''");
       console.log(`Scoring for ${songObserved} set at ${normedAnswerScore}, adding to answersWithScores`);
       answersWithScores.push({ fieldKey, songObserved, popularityObserved, normedAnswerScore, gridIdString });
       console.log("answersWithScores now at:");
