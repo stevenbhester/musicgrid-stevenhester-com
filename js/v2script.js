069e51d765ee1600da5a82f95f5c6e1e60dfa587
Steven Hester <45438379+stevenbhester@users.noreply.github.com>
2024-01-16 15:55:53 -0800

    Update v2script.js

--- js/v2script.js
+++ js/v2script.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMContentLoaded", function() {
   var hamburger = document.querySelector(".hamburger-menu");
   var mobileNav = document.querySelector(".mobile-nav");
@@ -10,9 +11,43 @@ document.addEventListener("DOMContentLoaded", function() {
     mobileNav.classList.toggle("show");
   });
 
+  var playButton = document.querySelector(".play-button");
+  var gridContainer = document.querySelector(".grid-container");
+  
+  // Play button event listener
+  playButton.addEventListener("click", function() {
+    gridContainer.classList.add("active");
+  });
+
   // Get all grid cells
   var cells = document.querySelectorAll(".grid-cell");
+  cells.forEach(function(cell) {
+    // Mouse enter event to highlight category and artist
+    cell.addEventListener("mouseenter", function() {
+      highlightRelated(this.dataset.category, this.dataset.artist);
+    });
+    // Mouse leave event to remove highlight
+    cell.addEventListener("mouseleave", function() {
+      removeHighlight(this.dataset.category, this.dataset.artist);
+    });
+  });
 
+  function highlightRelated(category, artist) {
+    // Highlight the related category and artist
+    var categoryElement = document.querySelector(".grid-category[data-category="" + category + ""]");
+    var artistElement = document.querySelector(".grid-artist[data-artist="" + artist + ""]");
+    if(categoryElement) categoryElement.classList.add("highlight");
+    if(artistElement) artistElement.classList.add("highlight");
+  }
+
+  function removeHighlight(category, artist) {
+    // Remove highlight from the related category and artist
+    var categoryElements = document.querySelectorAll(".grid-category.highlight, .grid-artist.highlight");
+    categoryElements.forEach(function(element) {
+      element.classList.remove("highlight");
+    });
+  }
+  
   // Add click event to each cell
   cells.forEach(function(cell) {
     cell.addEventListener("click", function() {
