4a21946fbe6ff7be3ed4570ac86bf67c6329fdc6
Steven Hester <45438379+stevenbhester@users.noreply.github.com>
2024-01-16 15:37:20 -0800

    Update v2script.js

--- js/v2script.js
+++ js/v2script.js
@@ -1,12 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var hamburger = document.querySelector('.hamburger-menu');
-    var mobileNav = document.querySelector('.mobile-nav');
+document.addEventListener("DOMContentLoaded", function() {
+  var hamburger = document.querySelector(".hamburger-menu");
+  var mobileNav = document.querySelector(".mobile-nav");
 });
 
-hamburger.addEventListener('click', function() {
-    // Toggles the 'open' class on the hamburger menu
-    this.classList.toggle('open');
+hamburger.addEventListener("click", function() {
+  // Toggles the "open" class on the hamburger menu
+  this.classList.toggle("open");
 
-    // Toggles the 'show' class on the mobile nav
-    mobileNav.classList.toggle('show');
+  // Toggles the "show" class on the mobile nav
+  mobileNav.classList.toggle("show");
 });
