a850607c236d480c46dc4b4f080214d99a38336f
Steven Hester <45438379+stevenbhester@users.noreply.github.com>
2024-01-16 21:05:39 -0800

    Update v2script.js

--- js/v2script.js
+++ js/v2script.js
@@ -26,7 +26,7 @@ function loadHeader() {
   const titleContainer = document.querySelector(".hero-content");
 
   //Adjust css for hero-content
-  titleContainer.style.marginTop= "7%";
+  titleContainer.style.marginTop= "9%";
   titleContainer.style.paddingTop= "7%";
   
   //Build Title
