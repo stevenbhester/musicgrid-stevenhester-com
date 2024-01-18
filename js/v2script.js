6176cf503541d5e7a49c4604b501f710525cf3c8
Steven Hester <45438379+stevenbhester@users.noreply.github.com>
2024-01-17 18:08:08 -0800

    Update v2script.js

--- js/v2script.js
+++ js/v2script.js
@@ -619,8 +619,8 @@ function highlightRelated(className) {
   if(classNames.length == 4) {
     var artistName = classNames[3];
     var catName = classNames[2];
-    var artistCellClass = `.cell.artistHeader.${artistName}`;
-    var genreCellClass = `.cell.genreHeader.${catName}`;
+    var artistCellClass = `.cell.artist.${artistName}`;
+    var genreCellClass = `.cell.genre-header.${catName}`;
     console.log("Highlighting relevancy for artist: '"+artistCellClass+"' and category '"+genreCellClass+"'");
     var categoryElement = document.querySelector(genreCellClass);
     var artistElement = document.querySelector(artistCellClass);
@@ -638,9 +638,9 @@ function removeHighlight(className) {
   if(classNames.length == 4) {
     var artistName = classNames[3];
     var catName = classNames[2];
-    var artistCellClass = `.cell.artistHeader.${artistName}`;
-    var genreCellClass = `.cell.genreHeader.${catName}`;
-    console.log("Highlighting relevancy for artist: '"+artistCellClass+"' and category '"+genreCellClass+"'");
+    var artistCellClass = `.cell.artist.${artistName}`;
+    var genreCellClass = `.cell.genre-header.${catName}`;
+    console.log("Dehighlighting relevancy for artist: '"+artistCellClass+"' and category '"+genreCellClass+"'");
     var categoryElement = document.querySelector(genreCellClass);
     var artistElement = document.querySelector(artistCellClass);
     console.log(categoryElement.innerText);
