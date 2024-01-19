bd3ad36300cc8411b520bc24c6bfae07bee8f2f3
Steven Hester <45438379+stevenbhester@users.noreply.github.com>
2024-01-19 15:37:35 -0800

    Update gb-artistSelect.js

--- js/gb-artistSelect.js
+++ js/gb-artistSelect.js
@@ -2,33 +2,41 @@ const sortableList = document.querySelector(".sortable-list");
 const items = sortableList.querySelectorAll(".item");
 
 items.forEach(item => {
-    item.addEventListener("dragstart", () => {
-        // Adding dragging class to item after a delay
-        setTimeout(() => item.classList.add("dragging"), 0);
-    });
-    item.addEventListener("touchmove", () => {
-        // Adding dragging class to item after a delay
-        setTimeout(() => item.classList.add("dragging"), 0);
-    });
-    // Removing dragging class from item on dragend event
-    item.addEventListener("dragend", () => item.classList.remove("dragging"));
-    item.addEventListener("touchend", () => item.classList.remove("dragging"));
+  item.addEventListener("dragstart", () => {
+    console.log("Dragging element "+item.textContent);
+    // Adding dragging class to item after a delay
+    setTimeout(() => item.classList.add("dragging"), 0);
+  });
+  item.addEventListener("touchmove", () => {
+    console.log("Dragging element "+item.textContent);
+    // Adding dragging class to item after a delay
+    setTimeout(() => item.classList.add("dragging"), 0);
+  });
+  // Removing dragging class from item on dragend event
+  item.addEventListener("dragend", () => item.classList.remove("dragging"));
+  item.addEventListener("touchend", () => item.classList.remove("dragging"));
 });
 
 const initSortableList = (e) => {
-    e.preventDefault();
-    const draggingItem = document.querySelector(".dragging");
-    // Getting all items except currently dragging and making array of them
-    let siblings = [...sortableList.querySelectorAll(".item:not(.dragging)")];
-
-    // Finding the sibling after which the dragging item should be placed
-    let nextSibling = siblings.find(sibling => {
-        return e.clientY <= sibling.offsetTop + sibling.offsetHeight / 2;
-    });
-
-    // Inserting the dragging item before the found sibling
-    sortableList.insertBefore(draggingItem, nextSibling);
-}
+  e.preventDefault();
+  console.log("initSortableList fired");
+  const draggingItem = document.querySelector(".dragging");
+  console.log("initSortableList detected on "+draggingItem.textContent);
+    
+  // Getting all items except currently dragging and making array of them
+  let siblings = [...sortableList.querySelectorAll(".item:not(.dragging)")];
+  console.log("siblings pulled");
+  
+  // Finding the sibling after which the dragging item should be placed
+  let nextSibling = siblings.find(sibling => {
+    console.log("Comparing drag element at "+e.clientY+" to sibling "+sibling.textContent+" at "+  sibling.offsetTop + sibling.offsetHeight / 2 + " (offsetTop: "+ sibling.offsetTop+"; offsetHeight/2: "+sibling.offsetHeight/2);
+    return e.clientY <= sibling.offsetTop + sibling.offsetHeight / 2;
+  });
+  console.log("Found match sibling "+nextSibling.textContent);
+  // Inserting the dragging item before the found sibling
+  sortableList.insertBefore(draggingItem, nextSibling);
+  console.log("Inserted drag element before found match");
+};
 
 
 sortableList.addEventListener("dragover", initSortableList);
