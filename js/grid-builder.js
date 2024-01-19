dec2ce3f2188dbca5bd6b8bf2ecd6f6bcc70a209
Steven Hester <45438379+stevenbhester@users.noreply.github.com>
2024-01-19 13:18:49 -0800

    Update grid-builder.js

--- js/grid-builder.js
+++ js/grid-builder.js
@@ -32,35 +32,8 @@ const currentToken = {
     localStorage.setItem('expires', expiry);
   }
 };
-
-// On page load, try to fetch auth code from current browser search URL
-const args = new URLSearchParams(window.location.search);
-const code = args.get('code');
-
-// If we find a code, we're in a callback, do a token exchange
-if (code) {
-  const token = await getToken(code);
-  currentToken.save(token);
-
-  // Remove code from URL so we can refresh correctly.
-  const url = new URL(window.location.href);
-  url.searchParams.delete("code");
-
-  const updatedUrl = url.search ? url.href : url.href.replace('?', '');
-  window.history.replaceState({}, document.title, updatedUrl);
-}
-
-// If we have a token, we're logged in, so fetch user data and render logged in template
-if (currentToken.access_token) {
-  const userData = await getUserData();
-  renderTemplate("main", "logged-in-template", userData);
-  renderTemplate("oauth", "oauth-template", currentToken);
-}
-
-// Otherwise we're not logged in, so render the login template
-if (!currentToken.access_token) {
-  renderTemplate("main", "login");
-}
+  
+renderTemplate("main", "login");
 
 async function redirectToSpotifyAuthorize() {
   const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
