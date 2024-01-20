ccc60645098513ef35d4efb283fe4436e3ecf417
Steven Hester <45438379+stevenbhester@users.noreply.github.com>
2024-01-19 19:52:28 -0800

    Update gb-artistSelect.js

--- js/gb-artistSelect.js
+++ js/gb-artistSelect.js
@@ -116,7 +116,7 @@ const initSortableList = (e) => {
   
   // Inserting the dragging item before the found sibling
   if(nextSibling) {
-    sortableList.insertBefore(draggingItem, nextSibling);
+    sortableList.insertBefore(draggingItem.get(0), nextSibling.get(0));
     console.log("Inserted drag element before next sibling");
   } else if (priorSibling) {
     sortableList.appendChild(draggingItem);
