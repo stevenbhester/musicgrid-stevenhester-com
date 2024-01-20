5f578209ed7e8bc4b0b5a13b63f2e347b7e3bb6a
Steven Hester <45438379+stevenbhester@users.noreply.github.com>
2024-01-19 16:36:00 -0800

    Update gb-artistSelect.js

--- js/gb-artistSelect.js
+++ js/gb-artistSelect.js
@@ -41,7 +41,8 @@ const initSortableList = (e) => {
       nextSibling = sibling;
     }
   });
-  console.log("Found prior sibling "+priorSibling.textContent.trim()+" and next sibling "+nextSibling.textContent.trim());
+  if(priorSibling) {console.log("Found prior sibling "+priorSibling.textContent.trim());} else {console.log("No prior sibling found");}
+  if(nextSibling) {console.log("Found next sibling "+nextSibling.textContent.trim())};} else {console.log("No next sibling found");}
   
   // Inserting the dragging item before the found sibling
   if(nextSibling) {
