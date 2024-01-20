6425fd912e7f4dfdb48b9703d66aad4328f5bcbf
Steven Hester <45438379+stevenbhester@users.noreply.github.com>
2024-01-19 18:31:15 -0800

    Update gb-artistSelect.js

--- js/gb-artistSelect.js
+++ js/gb-artistSelect.js
@@ -1,3 +1,74 @@
+// Start running code on page load
+window.onload = function() {
+  initializeSite();
+};
+
+// Log functionality and ask to fetch grid summary
+function initializeSite() {
+  console.log("Initializing Site");
+  console.log("Fetching Spotify OAUTH");
+  // Data structure that manages the current active token, caching it in localStorage
+  const currentToken = {
+    get access_token() { return localStorage.getItem("access_token") || null; },
+    get refresh_token() { return localStorage.getItem("refresh_token") || null; },
+    get expires_in() { return localStorage.getItem("refresh_in") || null; },
+    get expires() { return localStorage.getItem("expires") || null; },
+  };
+  fetchTopArtists(currentToken.accessToken);
+}
+
+//Get user data code (cool!)
+async function fetchTopArtists(aToken) {
+  try {
+    const response = await fetch("https://music-grid-io-42616e204fd3.herokuapp.com/fetch-top-artists", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ accessToken: aToken })
+    });
+
+    const data = await response.json();
+    buildArtistList(data);
+  } catch (error) {
+    console.error("Error encoding answers for grid:", error);
+  }
+}
+
+function buildArtistList(topArtistsData) {
+  const listContainer = document.getElementByClassName("sortable-list");
+  topArtistsData.forEach(artist => {
+    listContainer.appendChild(createArtistItem(artist.id,artist.name,artist.img));
+  }
+}
+
+function createArtistItem(id, name, img) {
+  const outerItem = document.createElement("li");
+  outerItem.classList.add("item");
+  outerItem.setAttribute("draggable", "true");
+  outerItem.setAttribute("data-artist-id", id);
+  
+  const artistImage = document.createElement("img");
+  artistImage.src = img;
+  
+  const innerDetails = document.createElement("div");
+  innerDetails.classList.add("details");
+  
+  const artistName = document.createElement("span");
+  artistName.textContent = name;
+
+  const dragDots = document.createElement("i");
+  dragDots.classList.add("uil", "uil-draggabledots");
+
+  innerDetails.appendChild(artistImage);
+  innerDetails.appendChild(artistName);
+  
+  outerItem.appendChild(innerDetails);
+  outerItem.appendChild(dragDots);
+}
+
+
+
+
+//Draggable code (blah)
 const sortableList = document.querySelector(".sortable-list");
 const items = sortableList.querySelectorAll(".item");
 
