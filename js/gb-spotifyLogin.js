aa93690f2b1e0eb35674a4eec0c670ce97c53d58
Steven Hester <45438379+stevenbhester@users.noreply.github.com>
2024-01-19 18:01:10 -0800

    Update gb-spotifyLogin.js

--- js/gb-spotifyLogin.js
+++ js/gb-spotifyLogin.js
@@ -32,9 +32,34 @@ const currentToken = {
     localStorage.setItem("expires", expiry);
   }
 };
-  
-renderTemplate("main", "login");
 
+const args = new URLSearchParams(window.location.search);
+const code = args.get('code');
+
+// If we find a code, we're in a callback, do a token exchange
+if (code) {
+  const token = await getToken(code);
+  currentToken.save(token);
+
+  // Remove code from URL so we can refresh correctly.
+  const url = new URL(window.location.href);
+  url.searchParams.delete("code");
+
+  const updatedUrl = url.search ? url.href : url.href.replace('?', '');
+  window.history.replaceState({}, document.title, updatedUrl);
+}
+
+// If we have a token, we're logged in, so fetch user data and render logged in template
+if (currentToken.access_token) {
+  const userData = await getUserData();
+  renderTemplate("main", "logged-in-template", userData);
+  renderTemplate("oauth", "oauth-template", currentToken);
+}
+
+// Otherwise we're not logged in, so render the login template
+if (!currentToken.access_token) {
+  renderTemplate("main", "login");
+}
 async function redirectToSpotifyAuthorize() {
   const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   const randomValues = crypto.getRandomValues(new Uint8Array(64));
