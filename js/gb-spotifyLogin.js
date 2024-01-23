1a456437be8d0544d15a3e6b786d6fea12b0aca4
Steven Hester <45438379+stevenbhester@users.noreply.github.com>
2024-01-22 19:10:34 -0800

    Update gb-spotifyLogin.js

--- js/gb-spotifyLogin.js
+++ js/gb-spotifyLogin.js
@@ -38,15 +38,7 @@ const code = args.get('code');
 
 // If we find a code, we're in a callback, do a token exchange
 if (code) {
-  const token = await getToken(code);
-  currentToken.save(token);
-
-  // Remove code from URL so we can refresh correctly.
-  const url = new URL(window.location.href);
-  url.searchParams.delete("code");
-
-  const updatedUrl = url.search ? url.href : url.href.replace('?', '');
-  window.history.replaceState({}, document.title, updatedUrl);
+  tokenHandler(code);
 }
 
 // If we have a token, we're logged in, so fetch user data and render logged in template
@@ -90,6 +82,16 @@ async function redirectToSpotifyAuthorize() {
   window.location.href = authUrl.toString(); // Redirect the user to the authorization server for login
 }
 
+async function tokenHandler(code) {
+  currentToken.save(token);
+  // Remove code from URL so we can refresh correctly.
+  const url = new URL(window.location.href);
+  url.searchParams.delete("code");
+
+  const updatedUrl = url.search ? url.href : url.href.replace('?', '');
+  window.history.replaceState({}, document.title, updatedUrl);}
+}
+
 // Soptify API Calls
 async function getToken(code) {
   const code_verifier = localStorage.getItem("code_verifier");
@@ -107,7 +109,7 @@ async function getToken(code) {
       code_verifier: code_verifier,
     }),
   });
-
+  
   return await response.json();
 }
 
