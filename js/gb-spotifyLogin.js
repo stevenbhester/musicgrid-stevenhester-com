968c06b77875651c715507968c1a71784a6c0595
Steven Hester <45438379+stevenbhester@users.noreply.github.com>
2024-07-26 16:47:24 -0700

    Update gb-spotifyLogin.js

--- js/gb-spotifyLogin.js
+++ js/gb-spotifyLogin.js
@@ -63,7 +63,7 @@ async function redirectToSpotifyAuthorize() {
 
   const code_verifier = randomString;
   const data = new TextEncoder().encode(code_verifier);
-  const hashed = await crypto.subtle.digest("SHA-256", data);
+  const hashed = await window.crypto.subtle.digest("SHA-256", data);
 
   const code_challenge_base64 = btoa(String.fromCharCode(...new Uint8Array(hashed)))
     .replace(/=/g, "")
