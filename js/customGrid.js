50931fa837c935630f4df3c4fd8c1687bc16edcf
Steven Hester <45438379+stevenbhester@users.noreply.github.com>
2024-07-26 17:46:58 -0700

    Update customGrid.js

--- js/customGrid.js
+++ js/customGrid.js
@@ -168,6 +168,7 @@ function buildGrid(data) {
   // Update Title
   newTitleText = document.getElementById("gridTitleCust");
   newTitleText.innerHTML = "<span style=\"font-size:2.3em;\">"+custGridTitle+"</span>"; 
+  document.title = gridTitleCust;
   
   // Create artist row
   const artistRow = document.createElement("div");
